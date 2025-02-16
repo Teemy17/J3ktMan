5758b0584fbb7298aa352535501b74d9b403d37e
Simmypeet <simmypeet@outlook.com>
2025-02-17 01:42:59 +0700

    add page to the project invitation

--- J3ktMan/J3ktMan.py
+++ J3ktMan/J3ktMan.py
@@ -3,6 +3,7 @@
 import reflex as rx
 
 from J3ktMan.page.index import index
+from J3ktMan.page.join_project import join_project
 from J3ktMan.page.signup import signup
 from J3ktMan.page.signin import signin
 
@@ -17,3 +18,5 @@ app.add_page(index)
 # for some reason, the @rx.page(route="...") decorator doesn't work so the route is added manually here :(
 app.add_page(signin, route="signin/[[...slug]]")
 app.add_page(signup, route="signup/[[...slug]]")
+
+app.add_page(join_project)
