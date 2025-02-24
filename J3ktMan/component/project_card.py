0a297c48d00a278c15084e205fdbaa344b0342ba
Teemy17 <teemy17@gmail.com>
2025-02-25 00:02:24 +0700

    feat: done basic project card component

--- J3ktMan/component/project_card.py
+++ J3ktMan/component/project_card.py
@@ -1,11 +1,24 @@
 import reflex as rx
 
-def project_card() -> rx.Component:
+def project_card(name: str, description: str, comments: int, files: int) -> rx.Component:
     return rx.card(
-        rx.flex(
-            rx.text("Project Name", size="5", weight="bold"),
-            rx.icon("move-vertical", color="gray"),
-            spacing="2",
+        rx.vstack(
+            rx.heading(name, size="3"),
+            rx.text(description, size="2", color="gray"),
+            rx.hstack(
+                rx.hstack(
+                    rx.icon("message-circle"),
+                    rx.text(f"{comments} comments", size="1"),
+                    rx.icon("file"),
+                    rx.text(f"{files} files", size="1"),
+                    spacing="2",
+                ),
+                align="center",
+            ),
+            spacing="4",
+            align="start",
         ),
-        rx.text("Project Description", size="3", color="gray"),
+        padding="4",
+        border_radius="lg",
+        shadow="md",
     )
--- J3ktMan/page/home.py
+++ J3ktMan/page/home.py
@@ -2,14 +2,50 @@ import reflex as rx
 from J3ktMan.component.project_card import project_card
 from J3ktMan.component.base import base_page
 
+# Sample project data
+projects = [
+    {
+        "name": "Project 1",
+        "description": "Lorem ipsum lodpr lamo jwjwj kekw\nsadad moel aal p apkda",
+        "comments": 12,
+        "files": 0,
+    },
+    {
+        "name": "Project 2",
+        "description": "Another project description example.",
+        "comments": 8,
+        "files": 2,
+    },
+    {
+        "name": "Project 3",
+        "description": "Lorem ipus.",
+        "comments": 5,
+        "files": 1,
+    },
+    {
+        "name": "Project 4",
+        "description": "Another project description example.",
+        "comments": 8,
+        "files": 2,
+    }
+]
+
 def home() -> rx.Component:
     return base_page(
         rx.vstack(
             rx.text("Home", size="7", weight="bold"),
             rx.grid(
-                project_card(),
+                rx.foreach(
+                    projects,
+                    lambda project: project_card(
+                        name=project["name"],
+                        description=project["description"],
+                        comments=project["comments"],
+                        files=project["files"],
+                    )
+                ),
                 columns="3",
-                spacing="3",
+                spacing="6",
                 align="start",
                 width="100%"
             )
