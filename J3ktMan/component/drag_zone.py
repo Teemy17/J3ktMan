168955a4af725e7a7febc4823cb21108d321d721
Simmypeet <simmypeet@outlook.com>
2025-03-05 13:40:26 +0700

    fix task dragging bug and add milestone assignment

--- J3ktMan/component/drag_zone.py
+++ J3ktMan/component/drag_zone.py
@@ -8,9 +8,11 @@ class DragZone(Box):
     A wrapper over `rx.box` allowing handling of drag events.
     """
 
-    on_drag_enter: rx.EventHandler[lambda: []]
-    on_drop: rx.EventHandler[lambda: []]
-    on_drag_leave: rx.EventHandler[lambda: []]
+    on_drag_enter: rx.EventHandler[lambda e: [e]]
+    on_drop: rx.EventHandler[lambda e: [e]]
+    on_drag_leave: rx.EventHandler[lambda e: [e]]
+    on_drag_over: rx.EventHandler[lambda e: [e]]
+    
 
 
 drag_zone = DragZone.create
