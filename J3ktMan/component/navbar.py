7a531bdc1631e60c4053f24deb87205231ead24a
Teemy17 <teemy17@gmail.com>
2025-04-04 16:22:39 +0700

    feat: change profile feature to use clerk user_button instead of another page
    user profile will appear as pop-up, also got logout menu so logout dialog is deleted

--- J3ktMan/component/navbar.py
+++ J3ktMan/component/navbar.py
@@ -1,12 +1,6 @@
 import reflex as rx
 import reflex_clerk as clerk
-
-
-class State(rx.State):
-    which_dialog_open: str = ""
-
-    def show_logout(self):
-        self.which_dialog_open = "logout"
+from reflex_clerk.lib.appearance import Appearance
 
 
 def logo() -> rx.Component:
@@ -18,33 +12,6 @@ def logo() -> rx.Component:
             on_click=rx.redirect("/home"),
             style={"cursor": "pointer"},  # type: ignore
         ),
-    )
-
-
-def logout_dialog() -> rx.Component:
-    return rx.dialog.root(
-        rx.dialog.content(
-            rx.dialog.title("Log out"),
-            rx.dialog.description(
-                "Are you sure you want to log out?",
-            ),
-            rx.flex(
-                rx.dialog.close(
-                    rx.button(
-                        "Cancel",
-                        color_scheme="gray",
-                        variant="soft",
-                    ),
-                ),
-                rx.dialog.close(
-                    clerk.sign_out_button(rx.button("Sign Out", color_scheme="red")),
-                ),
-                spacing="3",
-                justify="end",
-            ),
-        ),
-        open=State.which_dialog_open == "logout",
-        on_open_change=State.set_which_dialog_open(""),  # type: ignore
     )
 
 
@@ -84,37 +51,23 @@ def notification_icons() -> rx.Component:
 
 
 def user_profile() -> rx.Component:
-    return rx.menu.root(
-        rx.menu.trigger(
-            rx.hstack(
-                rx.avatar(
-                    name="Anima Agrawal",
-                    size="3",
-                    src=clerk.ClerkState.user.image_url,  # type: ignore
-                    border_radius="full",
+    return (
+        rx.hstack(
+            clerk.user_button(),
+            rx.vstack(
+                rx.text(
+                    clerk.ClerkState.user.username,  # type: ignore
+                    size="4",
+                    weight="medium",
                 ),
-                rx.vstack(
-                    rx.text(
-                        clerk.ClerkState.user.username,  # type: ignore
-                        size="4",
-                        weight="medium",
-                    ),
-                    align_items="start",
-                    justify="center",
-                    spacing="0",
-                    width="100%",
-                ),
-                rx.icon("chevron-down", color="gray"),
-                padding="4px",
-                border_radius="8px",
-                _hover={"background": "grey"},
+                align_items="start",
+                justify="center",
+                spacing="0",
+                width="100%",
             ),
-        ),
-        rx.menu.content(
-            rx.menu.item("Profile", on_click=rx.redirect("/profile")),
-            rx.menu.item("Settings"),
-            rx.menu.separator(),
-            rx.menu.item("Log out", on_click=State.show_logout),  # type: ignore
+            rx.icon("chevron-down", color="gray"),
+            padding="4px",
+            border_radius="8px",
         ),
     )
 
@@ -127,7 +80,6 @@ def navbar() -> rx.Component:
                 rx.spacer(),
                 notification_icons(),
                 user_profile(),
-                logout_dialog(),
                 width="100%",
                 padding="1em",
                 justify="between",
@@ -139,21 +91,7 @@ def navbar() -> rx.Component:
             rx.hstack(
                 rx.spacer(),
                 notification_icons(),
-                rx.menu.root(
-                    rx.menu.trigger(
-                        rx.avatar(
-                            name="Anima Agrawal",
-                            size="3",
-                            border_radius="full",
-                        ),
-                    ),
-                    rx.menu.content(
-                        rx.menu.item("Profile"),
-                        rx.menu.item("Settings"),
-                        rx.menu.separator(),
-                        rx.menu.item("Log out"),
-                    ),
-                ),
+                user_profile(),
                 width="100%",
                 padding="1em",
                 justify="between",
